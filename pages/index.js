193e07d0830e24530e70489e2a44393bad96d43d
Faanilo <faniloniainaa21@gmail.com>
2022-11-15 21:44:54 +0300

    v0.1

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import About from '../components/About'
+import Contact from '../components/Contact'
+import Main from '../components/Main'
+import Skills from '../components/Skills'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Faniloniaina RAMILISON</title>
+        <meta name="description" content="I’m a back-end web developer specializing in building (and occasionally designing) exceptional digital experiences." />  
+      </Head>
+    <Main />
+    <About />
+    <Skills />
+    <Contact />
+    </div>
+  )
+}
---
+++ pages/resume.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import Head from "next/head";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+
+const resume = () => {
+  return (
+    <>
+      <Head>
+        <title>Faniloniaina | Resume</title>
+        <meta
+          name="description"
+          content="I’m a Junior web developer specializing in building (and occasionally designing) exceptional digital experiences."
+        />
+        <link rel="icon" href="/fav.png" />
+      </Head>
+
+      <div className="max-w-[940px] mx-auto p-2 pt-[120px]">
+        <h2 className="text-center">Resume</h2>
+        <div className="bg-[#d0d4d6] my-4 p-4 w-full flex justify-between items-center">
+          <h2 className="text-center">Fanilo RAMILISON</h2>
+          <div className="flex">
+            <a
+              href="https://www.linkedin.com/in/faanilo-niaina-156014240/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaLinkedinIn size={20} style={{ marginRight: "1rem" }} />
+            </a>
+            <a
+              href="https://github.com/Faanilo"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaGithub size={20} style={{ marginRight: "1rem" }} />
+            </a>
+          </div>
+        </div>
+        <div className="text-center py-4 text-xl font-bold uppercase tracking-wider">
+          <div className="hidden sm:block">
+            <p>
+              <span className="px-1"></span> Web Development
+              <span className="px-1">|</span> Complex Problem Solving
+            </p>
+          </div>
+          <div className="block sm:hidden">
+            <p className="py-2">Web Development</p>
+            <p>Complex Problem Solving</p>
+          </div>
+        </div>
+        <p>Analytical, innovative, and motivated .I like challenges</p>
+
+        {/* Skills */}
+        <div className="text-center py-4">
+          <h5 className="text-center underline text-[18px] py-2">Skills</h5>
+          <p className="py-2">
+            <span className="font-bold">Technical Skills</span>
+            <span className="px-2">|</span>Back-End Web Developer
+            <span className="px-2">|</span> HTML
+            <span className="px-2">|</span>CSS
+            <span className="px-2">|</span>Javascript
+            <span className="px-2">|</span>React
+            <span className="px-2">|</span>Next JS
+            <span className="px-2">|</span>SQL
+            <span className="px-2">|</span>Tailwind
+            <span className="px-2">|</span>NodeJS
+            <span className="px-2">|</span>RESTAPI
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default resume;
